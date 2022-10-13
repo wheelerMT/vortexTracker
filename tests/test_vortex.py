2b4a92283e2b543494d92f5e68a85d112f02a872
Matt Wheeler <mattwheeler15@hotmail.co.uk>
2022-10-13 21:21:52 +0100

    Added new functions and tests

--- tests/test_vortex.py
+++ tests/test_vortex.py
@@ -1,6 +1,30 @@
+import pytest
 from tracker.vortex import Vortex
 
 
+@pytest.fixture
+def vortex():
+    return Vortex((0., 0.), 'sqv', 1)
+
+
 class TestVortex:
-    def test_instantiate_vortex(self):
-        assert Vortex((0., 0.), 'sqv', 1)
+    def test_instantiate_vortex(self, vortex):
+        assert vortex
+
+    def test_position(self, vortex):
+        assert vortex.position() == (0., 0.)
+
+    def test_type(self, vortex):
+        assert vortex.type() == 'sqv'
+
+    def test_winding(self, vortex):
+        assert vortex.winding() == 1
+
+    def test_uid(self, vortex):
+        assert vortex.id() is not None
+
+    @pytest.mark.parametrize("vortex_positions, expected_result",
+                             [((3., 4.), 5.), ((0., 0.), 0.), ((-5., 12.), 13.)])
+    def test_distance_to(self, vortex, vortex_positions, expected_result):
+        other_vortex = Vortex(vortex_positions, 'sqv', 1)
+        assert vortex.distance_to(other_vortex) == expected_result
--- tracker/vortex.py
+++ tracker/vortex.py
@@ -1,4 +1,5 @@
 import uuid
+import numpy as np
 
 
 class Vortex:
@@ -6,7 +7,23 @@ class Vortex:
 
     def __init__(self, pos: tuple[float, float], vortex_type: str, winding: int | float):
 
-        self.pos = pos
-        self.type = vortex_type
-        self.winding = winding
-        self.uid = f'{self.type}_{uuid.uuid4()}'
+        self._pos = pos
+        self._type = vortex_type
+        self._winding = winding
+        self._uid = f'{self._type}_{uuid.uuid4()}'
+
+    def position(self) -> tuple[float, float]:
+        return self._pos
+
+    def type(self):
+        return self._type
+
+    def winding(self):
+        return self._winding
+
+    def id(self) -> str:
+        return self._uid
+
+    def distance_to(self, vortex) -> float:
+        vortex_pos = vortex.position()
+        return np.sqrt((self._pos[0] - vortex_pos[0]) ** 2 + (self._pos[1] - vortex_pos[1]) ** 2)
